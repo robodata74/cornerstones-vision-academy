c741efb7362fded506ad20629fbbd43417aa2e68
robodata74 <147083077+robodata74@users.noreply.github.com>
2026-01-07 21:42:58 +0300

    feat: add dynamic gallery loader for all uploaded CVA images

--- js/gallery.js
+++ js/gallery.js
@@ -3,21 +3,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const galleryContainer = document.querySelector(".gallery-grid");
   const imageFolder = "images/gallery/";
-  
-  // List of images (you can add more files here)
+
+  // Actual list of images uploaded
   const galleryImages = [
-    "photo1.png",
-    "photo2.png",
-    "photo3.png",
-    "photo4.png",
-    "photo5.png",
+    "2.jpg", "5.jpg", "6.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg",
+    "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "20.jpg", "21.jpg",
+    "22.jpg", "23.jpg", "24.jpg", "26.jpg", "28.jpg", "29.jpg"
   ];
 
-  // Dynamically add images
+  // Dynamically add images to gallery container
   galleryImages.forEach((imgFile) => {
     const img = document.createElement("img");
     img.src = `${imageFolder}${imgFile}`;
     img.alt = "Cornerstones Vision Academy gallery image";
+    img.classList.add("gallery-item");
     galleryContainer.appendChild(img);
   });
 });
