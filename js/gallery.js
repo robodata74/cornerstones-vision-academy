5e1dd94d982edc82e693b4c58dfc81d8393e29f1
robodata74 <147083077+robodata74@users.noreply.github.com>
2026-01-08 00:12:25 +0300

    feat: added dynamic 3-column gallery, loads all images from images/gallery folder

--- js/gallery.js
+++ js/gallery.js
@@ -4,12 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const galleryContainer = document.querySelector(".gallery-grid");
   const imageFolder = "images/gallery/";
 
+  // List of gallery images
   const galleryImages = [
-    "2.jpg", "5.jpg", "6.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg",
-    "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "20.jpg", "21.jpg",
-    "22.jpg", "23.jpg", "24.jpg", "26.jpg", "28.jpg", "29.jpg"
+    "2.jpg","5.jpg","6.jpg","10.jpg","11.jpg","12.jpg","13.jpg",
+    "14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","20.jpg","21.jpg",
+    "22.jpg","23.jpg","24.jpg","26.jpg","28.jpg","29.jpg"
   ];
 
+  // Dynamically add images
   galleryImages.forEach((imgFile) => {
     const img = document.createElement("img");
     img.src = `${imageFolder}${imgFile}`;
