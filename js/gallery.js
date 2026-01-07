08387584310ffba56068583f995187b03b7c54c0
robodata74 <147083077+robodata74@users.noreply.github.com>
2026-01-08 00:48:25 +0300

    feat: dynamic responsive gallery with auto-loaded campus images

--- js/gallery.js
+++ js/gallery.js
@@ -1,22 +1,17 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const galleryContainer = document.querySelector(".gallery-grid");
-  const imageFolder = "images/gallery/";
-
-  // List of gallery images
-  const galleryImages = [
+  const container = document.querySelector(".gallery-grid");
+  const images = [
     "2.jpg","5.jpg","6.jpg","10.jpg","11.jpg","12.jpg","13.jpg",
     "14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","20.jpg","21.jpg",
     "22.jpg","23.jpg","24.jpg","26.jpg","28.jpg","29.jpg"
   ];
 
-  // Dynamically add images
-  galleryImages.forEach((imgFile) => {
+  images.forEach(file => {
     const img = document.createElement("img");
-    img.src = `${imageFolder}${imgFile}`;
-    img.alt = "Cornerstones Vision Academy gallery image";
-    img.classList.add("gallery-item");
-    galleryContainer.appendChild(img);
+    img.src = `images/gallery/${file}`;
+    img.alt = "Cornerstones Vision Academy campus";
+    container.appendChild(img);
   });
 });
